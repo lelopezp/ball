7dd19f6d5eea348c29b654a373d431cbd8c7533c
lelopezp <161893492+lelopezp@users.noreply.github.com>
2024-03-25 21:16:30 -0700

    simple window

--- main_game.py
+++ main_game.py
@@ -1,2 +1,14 @@
+import pygame
+
+
+def run() -> None:
+    pygame.init()
+
+    surface = pygame.display.set_mode((800, 700))
+
+    pygame.quit()
+
+
 if __name__ == "__main__":
     print("hello world");
+    run()
