a93210f42dcf47a5f7f3f27b2e90742b1e7d5e83
ahliu7 <landrew1018@gmail.com>
2024-03-26 11:39:34 -0700

    good morning pookie
    moved my code from the testing module to the main module, with annotations ofc. So far we have a ball that moves with the key that does not go out of bounds

--- main_game.py
+++ main_game.py
@@ -1,15 +1,53 @@
 import pygame
 
 
-def run() -> None:
+def main() -> None:
+
+    # stuff about the window
     pygame.init()
+    window_width = 400
+    window_height = 800
+    surface = pygame.display.set_mode((window_width, window_height))
+    background_color_r = 0
+    background_color_g = 0
+    background_color_b = 0
+    clock = pygame.time.Clock()
 
-    surface = pygame.display.set_mode((800, 700))
+    # stuff about the circle
+    circle_x = 200
+    circle_y = 50
+    circle_r = 12
+    circle_v = 1
 
+    # run loop
+    run = True
+    while run:
+
+        # how often window updates
+        clock.tick(300)
+
+        # draw background and circle
+        surface.fill(pygame.Color(background_color_r, background_color_g, background_color_b))
+        pygame.draw.circle(surface, pygame.Color(255, 255, 255), (circle_x, circle_y), circle_r)
+
+        # check for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # key controls 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT] and circle_x < window_width - circle_r:
+            circle_x += circle_v
+        if keys[pygame.K_LEFT] and circle_x > circle_r:
+            circle_x -= circle_v
+
+        # update window
+        pygame.display.flip()
+
+    # quit game
     pygame.quit()
 
 
 if __name__ == "__main__":
-    print("hello world")
-    run()
-
+    main()
